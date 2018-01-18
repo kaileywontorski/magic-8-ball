e042d191114c8aed5f769cd0f04df4fca4907491
kaileywontorski <kwontorski57@gmail.com>
2018-01-17 20:11:24 -0500

    edited questions

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-$(document).ready(function() {
-  var magic8Ball = {};
-  magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+$(document).ready(function(){
+var magic8Ball = {};
+magic8Ball.listofAnswers = ["What the heck are you thinking?", "Yes that may happen.", "Why'd you even ask that?", "Ask again.", "Without a doubt."];
 
-  magic8Ball.askQuestion = function(a) {
-    // var randomNumber = Math.random();
-    // var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
-    // var randomIndex = Math.floor(randomNumberForListOfAnswers);
-    var answer = this.listOfAnswers[Math.floor(Math.random() * this.listOfAnswers.length)];
+magic8Ball.askQuestion = function(a){
+	//var randomNumber = Math.random();
+	//var randomNumberAnswers = randomNumber * this.listofAnswers.length;
+	//var randomIndex = Math.floor(randomNumberAnswers);
+	var answer = this.listofAnswers[Math.floor(Math.random() * this.listofAnswers.length)];
 
-    $("#8ball").effect("shake");
-    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
-    $("#answer").text(answer);
-    $("#answer").fadeIn(4000);
+	$("#8ball").effect("shake");
+	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+	$("#answer").text( answer );
+	$("#answer").fadeIn( 4000 );
 
-    console.log(a);
-    console.log(answer);
-  };
+	console.log(a);
+	console.log(answer);
+};
 
-  console.log(magic8Ball);
+console.log(magic8Ball);
 
-  var onClick = function() {
-    $("#answer").hide();
-    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
-    setTimeout(function() {
-      var question = prompt("ASK A YES/NO QUESTION!");
-      magic8Ball.askQuestion("Hello?");
-    }, 500);
-  };
+var onClick = function() {
+	$("#answer").hide();	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+setTimeout(function() {
+    var question = prompt("ASK A YES/NO QUESTION!");
+    magic8Ball.askQuestion("Hello?");
+}, 500);
 
-  $("#answer").hide();
-
-  $("#questionButton").click(onClick);
+};
+	$("#answer").hide();
+	$("#questionButton").click( onClick );
 });
